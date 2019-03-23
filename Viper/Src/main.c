a53e194080cd7cac9ebc0bf51a4c403b0fbbe1ba
ALUGratka <gratkowska.ala@gmail.com>
2019-03-23 16:49:36 +0100

    diode

--- Viper/Src/main.c
+++ Viper/Src/main.c
@@ -34,33 +34,20 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-<<<<<<< HEAD
 #define CE		GPIO_PIN_13
 #define RST		GPIO_PIN_15 //reset - przy starcie stan 0 przez 100ns, potem 1 przy pracy
 #define DC 		GPIO_PIN_14 // 1 - przesylanie danych, 0 - komendy dla wyswietlacza
 
-#define GREEN_DIOD_ON 		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12,1)
-#define ORANGE_DIOD_ON	 	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13,1)
-#define RED_DIOD_ON			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14,1)
-#define BLUE_DIOD_ON		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15,1)
-#define GREEN_DIOD_OFF		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12,0)
-#define ORANGE_DIOD_OFF	 	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13,0)
-#define RED_DIOD_OFF		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14,0)
-#define BLUE_DIOD_OFF		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15,0)
-=======
-#define CE		GPIOE_PIN_10
-#define RST		GPIOE_PIN_11
-#define DC 		GPIOE_PIN_12
+#define GREEN_DIODE_ON 		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12,1)
+#define ORANGE_DIODE_ON	 	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13,1)
+#define RED_DIODE_ON			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14,1)
+#define BLUE_DIODE_ON		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15,1)
+#define GREEN_DIODE_OFF		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12,0)
+#define ORANGE_DIODE_OFF	 	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13,0)
+#define RED_DIODE_OFF		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14,0)
+#define BLUE_DIODE_OFF		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15,0)
 
-#define GREEN_DIODE_ON 		HAL_GPIO_WritePin(GPIOD, GPIOD_PIN_12,1);
-#define ORANGE_DIODE_ON	 	HAL_GPIO_WritePin(GPIOD, GPIOD_PIN_13,1);
-#define RED_DIODE_ON			HAL_GPIO_WritePin(GPIOD, GPIOD_PIN_14,1);
-#define BLUE_DIODE_ON		HAL_GPIO_WritePin(GPIOD, GPIOD_PIN_15,1);
-#define GREEN_DIODE_OFF		HAL_GPIO_WritePin(GPIOD, GPIOD_PIN_12,0);
-#define ORANGE_DIODE_OFF	 	HAL_GPIO_WritePin(GPIOD, GPIOD_PIN_13,0);
-#define RED_DIODE_OFF		HAL_GPIO_WritePin(GPIOD, GPIOD_PIN_14,0);
-#define BLUE_DIODE_OFF		HAL_GPIO_WritePin(GPIOD, GPIOD_PIN_15,0);
->>>>>>> a3a27f763a114bfc238740316309c130f7d1b960
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
