2e2e7c7cd1e293466574187775373eb3882299df
Weronika Kowalska <45520544+kowalskaw@users.noreply.github.com>
2019-03-26 11:53:34 +0100

    not significant changes

--- Viper/Src/Accelerometer.c
+++ Viper/Src/Accelerometer.c
@@ -1,36 +1,36 @@
-#include "main.h"
+#include "Defines.h"
 #include "Accelerometer.h"
 
 float accX, accY, accZ, out[4];
 
-void contorl(){
+void control(){
 	LIS3DSH_ReadACC(out);
 		  accX = out[0]/100;
 		  accY = out[1]/100;
 		  accZ = out[2]/100;
 
 		  if(accY>8&&accY<10){
-			  HAL_GPIO_WritePin(GPIOD,GPIO_PIN_12,1);
-			  HAL_GPIO_WritePin(GPIOD,GPIO_PIN_14,0);
-			  HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15,0);
-			  HAL_GPIO_WritePin(GPIOD,GPIO_PIN_13,0);
+			  GREEN_DIODE_ON;
+			  RED_DIODE_OFF;
+			  ORANGE_DIODE_OFF;
+			  BLUE_DIODE_OFF;
 		  }
 		  else if(accX>-9&&accX<-7){
-			  HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15,1);
-			  HAL_GPIO_WritePin(GPIOD,GPIO_PIN_12,0);
-			  HAL_GPIO_WritePin(GPIOD,GPIO_PIN_14,0);
-			  HAL_GPIO_WritePin(GPIOD,GPIO_PIN_13,0);
+			  GREEN_DIODE_OFF;
+			  RED_DIODE_OFF;
+			  ORANGE_DIODE_OFF;
+			  BLUE_DIODE_ON;
 		  }
 		  else if(accY>-9&&accY<-7){
-			  HAL_GPIO_WritePin(GPIOD,GPIO_PIN_14,1);
-			  HAL_GPIO_WritePin(GPIOD,GPIO_PIN_12,0);
-			  HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15,0);
-			  HAL_GPIO_WritePin(GPIOD,GPIO_PIN_13,0);
+			  GREEN_DIODE_OFF;
+			  RED_DIODE_ON;
+			  ORANGE_DIODE_OFF;
+			  BLUE_DIODE_OFF;
 		  }
 		  else if(accX>8&&accX<10){
-			  HAL_GPIO_WritePin(GPIOD,GPIO_PIN_13,1);
-			  HAL_GPIO_WritePin(GPIOD,GPIO_PIN_12,0);
-			  HAL_GPIO_WritePin(GPIOD,GPIO_PIN_14,0);
-			  HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15,0);
+			  GREEN_DIODE_OFF;
+			  RED_DIODE_OFF;
+			  ORANGE_DIODE_ON;
+			  BLUE_DIODE_OFF;
 		  }
 }
--- Viper/Src/main.c
+++ Viper/Src/main.c
@@ -114,7 +114,7 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  //control();
+	  control();
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
